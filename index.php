1741835f7f113f80ed37d64dc6917bbbeee89c9a
deusyvan <deusyvan@gmail.com>
2018-06-22 17:43:48 -0300

    Entendendo como funciona a herança

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+class Animal {
+    private $nome;
+    private $idade;
+}
 
+
+class Cavalo {
+    private $quantidade_de_patas;
+    private $tipo_de_pelo;
+}
+
+class Gato {
+    private $quantidade_de_patas;
+    private $miado;
+}
 
 ?>
