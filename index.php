647b4c7706e8cca9381a8ebb275fa4ec963492dd
deusyvan <deusyvan@gmail.com>
2018-06-22 18:09:44 -0300

    Iniciando estudo de interface (template)

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
 <?php
 
-abstract class Animal {
-    private $nome;
-    private $idade;
-    
-    abstract protected function andar();
-    
-    public function getNome(){
-        return $this->nome;
-    }
-
-    public function setNome($n){
-        $this->nome = $n;
-    }
-
-}
-
-
-class Cavalo extends Animal{
-    private $quantidade_de_patas;
-    private $tipo_de_pelo;
-    
-    public function andar(){
-        
-    }
-}
-
-
-$cavalo = new Cavalo();
-
-$cavalo->setNome("Cavalo Teste");
-
-echo "O meu cavalo é: ".$cavalo->getNome();
 
 ?>
