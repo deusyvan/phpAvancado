0dac77cec52e099700f138e05d9a7886856174ad
deusyvan <deusyvan@gmail.com>
2018-06-22 18:44:21 -0300

    Iniciando estudo de Namespace

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <?php
 
-class Animal{
-    
-    public function getNome(){
-        echo "getNome da classe Animal";
-    }
-    
-    public function testar(){
-        echo "Testado";
-    }
-    
-}
-
-class Cachorro extends Animal{
-    public function getNome(){
-        $this->testar();
-    }
-}
-
-$cachorro = new Cachorro();
-$cachorro->getNome();
 
 
 ?>
