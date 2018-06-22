b7823ea7cfe3b7efc64b1a0eeca9f79d1b0fdcb1
deusyvan <deusyvan@gmail.com>
2018-06-22 16:55:37 -0300

    Iniciando estudo de Herança de classes

--- index.php
+++ index.php
@@ -1,45 +1,5 @@
 <?php
 
-class  Post {
-    private $titulo;
-    private $data;
-    private $corpo;
-    private $comentarios;
-    private $qtComentarios;
-    
-    public function getTitulo(){
-        return $this->titulo;
-    }
 
-   
-    public function setTitulo($titulo){
-        if(is_string($titulo)){
-            $this->titulo = $titulo;
-        }
-    }
-
-    public function addComentario($msg){
-        $this->comentarios[] = $msg;
-        $this->contarComentarios();
-    }
-    
-    public function getQuantosComentarios() {
-        return $this->qtComentarios;
-    }
-    
-    private function contarComentarios(){
-        $this->qtComentarios = count($this->comentarios);
-    }
-    
-}
-
-$post = new Post();
-
-$post->addComentario("Teste");
-$post->addComentario("Teste 2");
-$post->addComentario("Teste 3");
-$post->addComentario("Teste 4");
-
-echo "Quantidade de comentários: ".$post->getQuantosComentarios();
 
 ?>
