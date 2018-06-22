bc0414256bc64f485299813dbf3c9d5595d28895
deusyvan <deusyvan@gmail.com>
2018-06-22 18:33:26 -0300

    Iniciando estudo de polimorfismo (subscrita de métodos)

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 
-interface Animal {
-    
-    public function andar();
-    
-}
 
-class Cachorro implements Animal{
-    
-    public function andar(){
-        echo "Estou andando...";
-    }
-}
-
-$cachorro = new Cachorro();
-
-$cachorro->andar();
 
 
 ?>
